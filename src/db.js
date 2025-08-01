f620e38bacda7a6357f09eeec641aea242ef092c
Leo <leo1073@outlook.es>
2025-08-01 16:00:27 -0300

    realizamos conexion con db

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import {createPool} from "mysql2" 
+
+export const pool = createPool(
+    {
+        host:"localhost",
+        user:"root",
+        password:"",
+        port:3306,
+        database: "redEducativaDb"
+    }
+)
+
